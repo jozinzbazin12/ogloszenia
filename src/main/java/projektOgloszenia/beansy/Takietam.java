c0f91e4f599ae54f191e50d168150730e2c30719
Grzegorz <jozin_z_bazin12@interia.pl>
2015-06-05 17:17:32 +0200

    Css fix

--- src/main/java/projektOgloszenia/beansy/Takietam.java
+++ src/main/java/projektOgloszenia/beansy/Takietam.java
@@ -2,15 +2,17 @@ package projektOgloszenia.beansy;
 
 import java.io.Serializable;
 
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
 @Singleton
 @Named("info")
 public class Takietam implements Serializable {
-		
+
 	private static final long serialVersionUID = 8231977590402640947L;
 	private String nazwa;
+	private String webPath = FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath();
 
 	public String getNazwa() {
 		return nazwa;
@@ -18,6 +20,11 @@ public class Takietam implements Serializable {
 
 	public Takietam() {
 		nazwa = "Serwis Og³oszeniowy ";
+
+	}
+
+	public String getWebPath() {
+		return webPath;
 	}
 
 }
