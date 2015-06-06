fa831652fae12454768bffbce92f10ab96ff51e3
Grzegorz <jozin_z_bazin12@interia.pl>
2015-06-06 23:19:05 +0200

    konfig log4j dzialczy

--- src/main/java/projektOgloszenia/beansy/Takietam.java
+++ src/main/java/projektOgloszenia/beansy/Takietam.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.inject.Singleton;
+import javax.servlet.ServletContext;
 
 import org.apache.log4j.xml.DOMConfigurator;
 
@@ -15,9 +16,6 @@ public class Takietam implements Serializable {
 	private static final long serialVersionUID = 8231977590402640947L;
 	private String nazwa;
 	private String webPath = FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath();
-	static {
-		DOMConfigurator.configure("src/main/resources/META-INF/log4j.xml");
-	}
 
 	public String getNazwa() {
 		return nazwa;
@@ -26,6 +24,8 @@ public class Takietam implements Serializable {
 	public Takietam() {
 		nazwa = "Serwis Og³oszeniowy ";
 		webPath = FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath();
+		String absoluteDiskPath = ((ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext()).getRealPath("WEB-INF/classes/META-INF/log4j.xml");
+		DOMConfigurator.configure(absoluteDiskPath);
 	}
 
 	public String getWebPath() {
