29a4fa45c4b7cb71a37125f2c9a9809dc1d7ddc4
Rayssadiel <aneta.klis@gmail.com>
2015-06-06 21:29:31 +0200

    Update import.sql

--- import.sql
+++ import.sql
@@ -5,35 +5,35 @@ INSERT INTO `ustawienie`(`nazwa`, `wartosc`) VALUES ("ile_plikow",3);
 INSERT INTO `ustawienie`(`nazwa`, `wartosc`) VALUES ("rozmiar",5555);
 
 INSERT INTO `kategoria` (`ID`, `nazwa`, `ojciec_id`) VALUES
-(1, 'Motoryzacja', 0),
-(2, 'Nieruchomoœci', 0),
-(3, 'Zabawki', 0),
-(4, 'Elektronika', 0),
-(5, 'Us³ugi', 0),
+(1, 'Motoryzacja', null),
+(2, 'NieruchomoÅ“ci', null),
+(3, 'Zabawki', null),
+(4, 'Elektronika', null),
+(5, 'UsÂ³ugi', null),
 (6, 'Samochody', 1),
 (7, 'Motocykle', 1),
 (8, 'Osobowe', 6),
-(9, 'Ciê¿arowe', 6),
-(12, 'Meble', 0),
-(13, 'Krzes³a', 12),
+(9, 'CiÃªÂ¿arowe', 6),
+(12, 'Meble', null),
+(13, 'KrzesÂ³a', 12),
 (14, 'Szafy', 12),
-(15, 'Sto³y', 12),
+(15, 'StoÂ³y', 12),
 (16, 'Sportowe', 6);
 
 INSERT INTO `slowo` (`slowo`) VALUES
-('dópa'),
-('dópe'),
+('dÃ³pa'),
+('dÃ³pe'),
 ('dupa'),
 ('dupe'),
-('gówno'),
+('gÃ³wno'),
 ('gunwo'),
 ('guwno'),
-('kótas'),
+('kÃ³tas'),
 ('kutas');
 
 INSERT INTO `notatka` (`id`, `tresc`) VALUES
-(2, 'Jakaœtam notatka'),
-(3, 'Jakaœ inna notatka'),
+(2, 'JakaÅ“tam notatka'),
+(3, 'JakaÅ“ inna notatka'),
 (8, 'sdasdasdasdasdasdasdasdadasdad a dasd asd asdada'),
 (9, 'Dupa XD'),
 (10, 'asdadsasdfsadasdasdasdadfsdfs');
